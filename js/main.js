fb75e13a7a323355dd48ad4fe478dd11c0d805cf
Braden <braden337@gmail.com>
2016-03-13 17:55:17 -0500

    refactor random bg

--- js/main.js
+++ js/main.js
@@ -3,16 +3,12 @@ var count = 0;
 $('#tapper').click(function() {
   count++;
   $(this).text(count);
-  $(this).css(randomBg());
+  randomBg(this);
 });
 
-function randomBg() {
+function randomBg(element) {
   var hue = Math.floor(Math.random()*360+1);
   var bgColor = 'hsla(' + hue + ', 50%, 50%, 1)';
   var color = 'hsla(' + hue + ', 50%, 30%, 1)';
-  return { backgroundColor: bgColor, color:  color};
+  $(element).css({backgroundColor: bgColor, color:  color});
 }
-
-$(document).ready(function() {
-  colorize();
-});
